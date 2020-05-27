3b7601de5780c433bfd772e89050d40a808b46c9
Korn <bo.sutthatat_st@tni.ac.th>
2020-05-27 16:36:41 +0700

    Use axios

--- src/pages/ArchivePage.vue
+++ src/pages/ArchivePage.vue
@@ -3,31 +3,21 @@
 </template>
 
 <script>
+import axios from 'axios'
 import CardList from '../components/CardList.vue'
 export default {
     name: 'ArchivePage',
     components: {
         CardList
     },
+    async created(){
+        const {data} = await axios.get('http://localhost:3000/archives')
+        this.archives = data
+    },
     data(){
         return{
             archives:[
-                {
-                    date: "25-05-2020",
-                    time: "17:00",
-                    tasks:[
-                        {id: 1, title: "Item A", status: "done" },
-                        {id: 2, title: "Item B", status: "done" },
-                    ]
-                },
-                {
-                    date: "26-05-2020",
-                    time: "17:00",
-                    tasks:[
-                        {id: 3, title: "Item C", status: "done" },
-                        {id: 4, title: "Item D", status: "done" },
-                    ]
-                }
+                
             ]
         }
     }
